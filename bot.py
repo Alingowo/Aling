49e0f82bbd1775eaf5bc66d7e0434dc2caee698e
Alingowo <121618100+Alingowo@users.noreply.github.com>
2023-01-05 23:59:52 +0800

    Aling-1

--- bot.py
+++ bot.py
@@ -22,4 +22,4 @@ async def on_member_remove(member):
     channel = bot.get_channel(1060215095893954691)
     await channel.send(f'{member}leave!')
 
-bot.run('MTA1Nzg2ODYzNjExNjc1NDQ3Mw.GIsBMZ.EJr3O3kWJev1DiOwYaBngyf_mM4k7BiPjbYZv4')
+bot.run('MTA1Nzg2ODYzNjExNjc1NDQ3Mw.G-_FH2.Onj0W_7gpF04Q7M8DHqz-cYpyCgRNLn11tIlSk')
