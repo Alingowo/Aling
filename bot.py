210f73edb46ef76ae954c46d8fd943a2adc12531
Alingowo <121618100+Alingowo@users.noreply.github.com>
2023-01-05 23:58:33 +0800

    Aling

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='[',intents= intents )
+bot = commands.Bot(intents = discord.Intents.all(), command_prefix='[')
 
 @bot.event
 
@@ -22,4 +22,4 @@ async def on_member_remove(member):
     channel = bot.get_channel(1060215095893954691)
     await channel.send(f'{member}leave!')
 
-bot.run('MTA1Nzg2ODYzNjExNjc1NDQ3Mw.G___LA.S6QTphQDu5Q0xGpupZC8t0jOMvtY-DNGzkgJ7o')
+bot.run('MTA1Nzg2ODYzNjExNjc1NDQ3Mw.GIsBMZ.EJr3O3kWJev1DiOwYaBngyf_mM4k7BiPjbYZv4')
